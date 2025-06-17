9b3956d3bc08408472d44e2405351352528a29ce
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-17 16:31:09 +1000

    testing models and updating facade

--- app/services/facade.py
+++ app/services/facade.py
@@ -11,11 +11,19 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
+    # --- CRU User ---
     def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
 
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
+
+    def get_user_by_email(self, email):
+        return self.user_repo.get_by_attribute('email', email)
+
+    # --- CRU Place
     def create_place(self, title, description, price, latitude, longitude, owner):
         # Creates the place by calling the model/class we created
         place = Place(title=title, description=description, price=price,
@@ -40,6 +48,7 @@ class HBnBFacade:
         # Placeholder for logic to update a place
         pass
 
+    # --- CRU Amenity ---
     def create_amenity(self, amenity_data):
     # Placeholder for logic to create an amenity
         pass
@@ -54,4 +63,29 @@ class HBnBFacade:
 
     def update_amenity(self, amenity_id, amenity_data):
         # Placeholder for logic to update an amenity
+        pass
+
+    # --- CRU Review ---
+    def create_review(self, review_data):
+    # Placeholder for logic to create a review, including validation for user_id, place_id, and rating
+        pass
+
+    def get_review(self, review_id):
+        # Placeholder for logic to retrieve a review by ID
+        pass
+
+    def get_all_reviews(self):
+        # Placeholder for logic to retrieve all reviews
+        pass
+
+    def get_reviews_by_place(self, place_id):
+        # Placeholder for logic to retrieve all reviews for a specific place
+        pass
+
+    def update_review(self, review_id, review_data):
+        # Placeholder for logic to update a review
+        pass
+
+    def delete_review(self, review_id):
+        # Placeholder for logic to delete a review
         pass

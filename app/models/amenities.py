fb0cd42376cecdbc96c9a2d2c6efe1d3713283f0
cam.g.timms@gmail.com <cam.g.timms@gmail.com>
2025-06-17 11:32:24 +1000

    added getters and setters to amenities

--- app/models/amenities.py
+++ app/models/amenities.py
@@ -7,3 +7,19 @@ class Place(BaseModel):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    # --- Getters and Setters ---
+    @property
+    def name(self):
+        """ Returns value of the amenity name"""
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        """Setter for prop title"""
+        # ensure that the value is up to 100 alphabets only after removing excess white-space
+        is_valid_name = 0 < len(value.strip()) <= 50
+        if is_valid_name:
+            self._name = value.strip()
+        else:
+            raise ValueError("Invalid title length!")

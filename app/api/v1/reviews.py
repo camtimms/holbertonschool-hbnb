8a0e4d13c58a2cca2b54560ec28a20b363811dc6
kajxcode <kaj@gmail.com>
2025-06-18 14:48:20 +1000

    added facade and review api logic

--- app/api/v1/reviews.py
+++ app/api/v1/reviews.py
@@ -1,4 +1,5 @@
 from flask_restx import Namespace, Resource, fields
+from flask import request
 from app.services import facade
 
 api = Namespace('reviews', description='Review operations')
@@ -18,14 +19,18 @@ class ReviewList(Resource):
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new review"""
-        # Placeholder for the logic to register a new review
-        pass
+        try:
+            review_data = request.json
+            new_review = facade.create_review(review_data)
+            return new_review, 201
+        except ValueError as e:
+            return {'error': str(e)}, 400
 
     @api.response(200, 'List of reviews retrieved successfully')
     def get(self):
         """Retrieve a list of all reviews"""
-        # Placeholder for logic to return a list of all reviews
-        pass
+        reviews = facade.get_all_reviews()
+        return reviews, 200
 
 @api.route('/<review_id>')
 class ReviewResource(Resource):
@@ -33,8 +38,10 @@ class ReviewResource(Resource):
     @api.response(404, 'Review not found')
     def get(self, review_id):
         """Get review details by ID"""
-        # Placeholder for the logic to retrieve a review by ID
-        pass
+        review = facade.get_review(review_id)
+        if review:
+            return review, 200
+        return {'error': 'Review not found'}, 404
 
     @api.expect(review_model)
     @api.response(200, 'Review updated successfully')
@@ -42,8 +49,15 @@ class ReviewResource(Resource):
     @api.response(400, 'Invalid input data')
     def put(self, review_id):
         """Update a review's information"""
-        # Placeholder for the logic to update a review by ID
-        pass
+        review_data = request.json
+        try:
+            updated_review = facade.update_review(review_id, review_data)
+            if updated_review:
+                return updated_review, 200
+            return {'error': 'Review not found'}, 404
+        except ValueError as e:
+            return {'error': str(e)}, 400
+    
 
     @api.response(200, 'Review deleted successfully')
     @api.response(404, 'Review not found')
--- app/services/facade.py
+++ app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+import uuid
 
 class HBnBFacade:
     def __init__(self):
@@ -43,3 +44,55 @@ class HBnBFacade:
     def update_amenity(self, amenity_id, amenity_data):
         # Placeholder for logic to update an amenity
         pass
+
+    def create_review(self, review_data):
+        #validate required fields
+        required_fields = ['user_id', 'place_id', 'rating', 'text']
+        for field in required_fields:
+            if field not in review_data:
+                return ValueError(f"Missing field: {field}")
+        
+        #validate user and place id
+        if not self.user_repo.get(review_data['user_id']):
+            raise ValueError("Invalid user_id")
+        if not self.place_repo.get(review_data['palce_id']):
+            raise ValueError("invalid place_id")
+        #valdiate rating
+        rating = review_data['rating']
+        if not isinstance(rating, (int, float)) or not (1 <= rating <= 5):
+            raise ValueError("Rating must be between 1 and 5")
+        #create id for review
+        review_id = str(uuid.uuid4())
+        review_data['id'] = review_id
+
+        self.review_repo.add(review_data)
+        return review_data
+
+    def get_review(self, review_id):
+        return self.review_repo.get(review_id)
+
+    def get_all_reviews(self):
+        return self.review_repo.get_all()
+
+    def get_reviews_by_place(self, place_id):
+       return [review for review in self.review_repo.get_all() if review['plade_id'] is place_id]
+
+    def update_review(self, review_id, review_data):
+        review = self.review_repo.get(review_id)
+        if not review:
+            return None
+
+        #only updates change for text and rating
+        if 'rating' in review_data:
+            new_rating = review_data['rating']
+            if not isinstance(new_rating, (int, float)) or not (1 <= new_rating <= 5):
+                raise ValueError("Rating must be between 1 and 5")
+            review['rating'] = new_rating
+
+        if 'text' in review_data:
+            review['text'] = review_data['text']
+
+        return review
+
+    def delete_review(self, review_id):
+        return self.review_repo.delete(review_id)

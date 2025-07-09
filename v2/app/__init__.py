a8e2bc5c864de4c1b987f868a6a961b1d4b3ac27
kajxcode <kaj@gmail.com>
2025-07-09 16:13:07 +1000

    added auth file and session based authentication

--- v2/app/__init__.py
+++ v2/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_session import Session
+from datetime import timedelta
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
@@ -11,22 +13,32 @@ def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    #Session configuration
+    app.secret_key = 'secretkey'
+    app.config['SESSION_TYPE'] = 'sqlalchemy'
+    app.config['SESSION_SQLALCHEMY'] = db
+    app.config['SESSION_PERMANENT'] = True
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30 ) #expiration of session
+
     # Initialize Flask extensions with app
     bcrypt.init_app(app)
     db.init_app(app)
+    Session(app)
 
-    # Crate Api instance
+    # Create Api instance
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     from app.api.v2.users import api as users_ns
     from app.api.v2.amenities import api as amenities_ns
     from app.api.v2.places import api as places_ns
     from app.api.v2.reviews import api as reviews_ns
+    from app.api.v2.auth import auth_api #being added auth file
 
     api.add_namespace(users_ns, path="/api/v1/users")
     api.add_namespace(amenities_ns, path="/api/v1/amenities")
     api.add_namespace(places_ns, path="/api/v1/places")
     api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(auth_api, path="/api/v1/auth") #added name space for auth
 
     # Create database tables within app context
     with app.app_context():
--- v2/app/api/v2/users.py
+++ v2/app/api/v2/users.py
@@ -26,7 +26,7 @@ class UserList(Resource):
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
-    
+
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
 

e4bee55f71c88f70ba3f383d8d4cc6e96a2e3ae7
camtimms <cam.g.timms@gmail.com>
2025-08-14 20:29:55 +1000

    changed namespace from v1 to v3

--- v3/app/__init__.py
+++ v3/app/__init__.py
@@ -18,17 +18,17 @@ def create_app(config_class="config.DevelopmentConfig"):
     db.init_app(app)
 
     # Crate Api instance
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v3/')
 
     from app.api.v3.users import api as users_ns
     from app.api.v3.amenities import api as amenities_ns
     from app.api.v3.places import api as places_ns
     from app.api.v3.reviews import api as reviews_ns
 
-    api.add_namespace(users_ns, path="/api/v1/users")
-    api.add_namespace(amenities_ns, path="/api/v1/amenities")
-    api.add_namespace(places_ns, path="/api/v1/places")
-    api.add_namespace(reviews_ns, path="/api/v1/reviews")
+    api.add_namespace(users_ns, path="/api/v3/users")
+    api.add_namespace(amenities_ns, path="/api/v3/amenities")
+    api.add_namespace(places_ns, path="/api/v3/places")
+    api.add_namespace(reviews_ns, path="/api/v3/reviews")
 
     # Create database tables within app context
     with app.app_context():
